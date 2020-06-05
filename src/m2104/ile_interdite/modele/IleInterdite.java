cd6bd0fc8e8f902f663217f027a0a3537a9ca6d9
pappamat <mat.papp@free.fr>
2020-06-05 14:45:52 +0200

    gitignore

--- src/m2104/ile_interdite/modele/IleInterdite.java
+++ src/m2104/ile_interdite/modele/IleInterdite.java
@@ -1,5 +1,6 @@
 package m2104.ile_interdite.modele;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import m2104.ile_interdite.util.Message;
 import patterns.observateur.Observable;
@@ -10,6 +11,12 @@ import patterns.observateur.Observateur;
  * @author Raphaël Bleuse <raphael.bleuse@iut2.univ-grenoble-alpes.fr>
  */
 public class IleInterdite extends Observable<Message> {
+    private int curseur;
+    private Grille grille;
+    private ArrayList<Aventurier> aventuriers;
+    private Deck deckTresor;
+    private Deck deckInnondation;
+    
     public IleInterdite(Observateur<Message> observateur) {
         this.addObservateur(observateur);
     }
@@ -19,5 +26,33 @@ public class IleInterdite extends Observable<Message> {
         String[] nomAventuriers = new String[nbJoueurs];
         Arrays.fill(nomAventuriers, "Aventurier");
         return nomAventuriers;
+    }
+
+    public int getCurseur() {
+        return curseur;
+    }
+
+    public void setCurseur(int curseur) {
+        this.curseur = curseur;
+    }
+
+    public Grille getGrille() {
+        return grille;
+    }
+
+    public ArrayList<Aventurier> getAventuriers() {
+        return aventuriers;
+    }
+
+    public Deck getDeckTresor() {
+        return deckTresor;
+    }
+
+    public Deck getDeckInnondation() {
+        return deckInnondation;
+    }
+
+    public void initGrille() {
+
     }
 }

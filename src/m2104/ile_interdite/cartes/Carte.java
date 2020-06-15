84185070286d29c2d8cba844c327ab16e9933692
Tanguy Signoret <tanguy.signoret@etu.univ-grenoble-alpes.fr>
2020-06-15 09:57:02 +0200

    Maj de Carte

--- src/m2104/ile_interdite/cartes/Carte.java
+++ src/m2104/ile_interdite/cartes/Carte.java
@@ -11,13 +11,16 @@ package m2104.ile_interdite.cartes;
  */
 public abstract class Carte {
     private Deck deck;
+    private Aventurier aventurier;
 	
-    public Carte() {
+    public Carte(Aventurier aventurier) {
         this.deck = null;
+        this.aventurier = aventurier;
     }
 	
     public Carte(Deck deck) {
         this.deck = deck;
+        this.aventurier = null;
     }
 
     public Deck getDeck(){
@@ -26,6 +29,14 @@ public abstract class Carte {
 
     public void setDeck(Deck newDeck){
         this.deck = newDeck;
+    }
+
+    public Aventurier getAventurier(){
+        return this.aventurier;
+    }
+
+    public void setAventurier(Aventurier aventurier){
+        this.aventurier = aventurier;
     }
     
     public abstract void action();  

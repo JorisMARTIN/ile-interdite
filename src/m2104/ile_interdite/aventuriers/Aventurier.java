f6d1c628c3f766238df9c692c8031d2425c03283
Thomas Jhistarry <thomas.jhistarry@etu.univ-grenoble-alpes.fr>
2020-06-16 08:37:02 +0200

    Correction méthode peutSeDeplacer

--- src/m2104/ile_interdite/aventuriers/Aventurier.java
+++ src/m2104/ile_interdite/aventuriers/Aventurier.java
@@ -59,7 +59,7 @@ public abstract class Aventurier {
         boolean peutSeDeplacer = true;
         int indexTuileCible = this.ileInterdite.getGrille().getTuiles(true).indexOf(tuile);
         int indexTuileActuelle = this.ileInterdite.getGrille().getTuiles(true).indexOf(this.position);
-        if (indexTuileCible == indexTuileActuelle) {
+        if (indexTuileCible == indexTuileActuelle || tuile == null) {
             peutSeDeplacer = false;
         } else if ((indexTuileActuelle < 29 && indexTuileActuelle + 6 == indexTuileCible)
              || (indexTuileActuelle < 35 && indexTuileActuelle + 1 == indexTuileCible)

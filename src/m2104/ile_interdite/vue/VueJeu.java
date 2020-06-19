66b091bdd327e95f59e8fc8f92cf72c35c315306
pappamat <mat.papp@free.fr>
2020-06-19 13:55:29 +0200

    showOnScreen

--- src/m2104/ile_interdite/vue/VueJeu.java
+++ src/m2104/ile_interdite/vue/VueJeu.java
@@ -82,7 +82,7 @@ public class VueJeu {
         this.affGrille();
 
         fenetre.add(grillePanel);
-        
+
         fenetre.setVisible(true);
     }
 
@@ -137,7 +137,6 @@ public class VueJeu {
      * @param idAventurier : Le numero de l'aventurier demandant l'action
      */
     public void surbrillerTuiles(ArrayList<Boolean> possibilites, Utils.Pion pion, int action, int idAventurier) {
-        int aventurierADeplacer = idAventurier;
         for (int i = 0; i < grille.getTuiles(true).size(); i++) {
 
             JButton bouton = this.boutons.get(i);

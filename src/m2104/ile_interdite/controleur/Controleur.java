f2d21a1029b8dc06e7b863249851b1f353317dd4
pappamat <mat.papp@free.fr>
2020-06-19 14:50:54 +0200

    fix assecher

--- src/m2104/ile_interdite/controleur/Controleur.java
+++ src/m2104/ile_interdite/controleur/Controleur.java
@@ -10,6 +10,7 @@ import m2104.ile_interdite.modele.Tuile;
 import m2104.ile_interdite.util.Message;
 import m2104.ile_interdite.util.Parameters;
 import m2104.ile_interdite.vue.IHM;
+import m2104.ile_interdite.vue.VueAventurier;
 import patterns.observateur.Observateur;
 
 /**
@@ -115,13 +116,14 @@ public class Controleur implements Observateur<Message> {
                 break;
 
             case ASSECHER:
+                VueAventurier vueA = this.ihm.getVueAventuriers().get(msg.idAventurier);
+                vueA.activerBoutons(true, true, true, true, false, vueA.getNomAventurier() == "Navigateur", true);
                 Tuile tuile = this.ileInterdite.getGrille().getTuile(msg.nomTuile);
                 if (msg.action == 1)
                     this.ileInterdite.getAventuriers().get(msg.idAventurier).assecher(tuile);
                 else
                     tuile.setEtat(EtatTuile.NORMAL);
                 this.ihm.majVueJeu();
-                this.ihm.activerActions(msg.idAventurier, true, true, true, true, false, this.ihm.getVueAventuriers().get(msg.idAventurier).getNomAventurier() == "Navigateur", true);
                 break;
 
             case BOUGER:
@@ -192,8 +194,9 @@ public class Controleur implements Observateur<Message> {
                 break;
                 
             case FIN_DON:
-                this.ihm.getVueAventuriers().get(msg.idAventurier).setDescription(msg.isReussi == true ? "Don effectué !" : "Erreur, le don n'a\npas pu être effectué !");
-                this.ihm.getVueAventuriers().get(msg.idAventurier).activerBoutons(true, true, true, true, false, true, true);
+                VueAventurier va = this.ihm.getVueAventuriers().get(msg.idAventurier);
+                va.setDescription(msg.isReussi == true ? "Don effectué !" : "Erreur, le don n'a\npas pu être effectué !");
+                va.activerBoutons(true, true, true, true, va.getNomAventurier() == "Navigateur", true, true);
                 break;
                 
             case DEPLACER_HELICO:
